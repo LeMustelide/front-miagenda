7d5cdba111c183dc8e708009906002451e87b7a2
Marcoul <marcvirgili.contact@gmail.com>
2024-08-24 12:34:19 +0200

    refactor: update API URL in TimetableService

--- src/app/services/timetable.service.ts
+++ src/app/services/timetable.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TimetableService {
-  private apiUrl = 'http://localhost/fetch_schedule';
+  private apiUrl = 'https://api.miagenda.fr/fetch_schedule';
 
   constructor(private http: HttpClient) { }
 
